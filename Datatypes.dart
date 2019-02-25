9c7a3dd0ee84f405bc09706602a1e6497b05663a
nipun sharma <30398542+nipun-sharma123@users.noreply.github.com>
2019-02-25 22:19:47 +0530

    Create Datatypes.dart
    Datatypes, Variables, boolean

---
+++ Datatypes.dart
@@ -0,0 +1,17 @@
+void main() {
+  //Numbers : int
+  int score = 23;
+  
+  //Numbers : Double 
+  double percentage = 91.2;
+
+  //Strings
+  String name = "nipun";
+  
+  //Boolean
+  bool isValid = true;
+  
+  
+  print(score);
+  print(isValid);
+}
